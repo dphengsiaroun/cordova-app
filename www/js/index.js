9fb152d105c2422ca0479b8e116f306bd2c55f48
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-06-29 14:59:04 +0200

    add: material design

--- www/js/index.js
+++ www/js/index.js
@@ -47,3 +47,17 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+
+function PrendreUnePhoto(){
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI });
+}
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
